a43fd4ffa42137fd8b9634d637250a9c43d9f9ad
Laura Marshall <L_H_Marshall@yahoo.co.uk>
2013-08-08 15:57:06 +0200

    New Class: Line Transect Parallel Systematic Design

---
+++ DSsim/R/LT.Systematic.Design.R
@@ -0,0 +1,7 @@
+################################################################################
+# CONSTRUCT CLASS AND DEFINE INITIALIZE AND VALIDITY
+################################################################################
+
+setClass(Class = "LT.Systematic.Design", 
+         contains = "LT.Design"
+) 
