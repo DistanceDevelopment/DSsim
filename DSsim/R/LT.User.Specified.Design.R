0515cbb2a0eedabc8b02e691d42a8555fcd3084d
Laura Marshall <L_H_Marshall@yahoo.co.uk>
2013-08-08 15:57:56 +0200

    New Class: User specified / created design only to be created from existing shapefile.

---
+++ DSsim/R/LT.User.Specified.Design.R
@@ -0,0 +1,7 @@
+################################################################################
+# CONSTRUCT CLASS AND DEFINE INITIALIZE AND VALIDITY
+################################################################################
+
+setClass(Class = "LT.User.Specified.Design", 
+         contains = "LT.Design"
+) 
