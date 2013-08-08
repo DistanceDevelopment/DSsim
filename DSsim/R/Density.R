deb04f221719790c04ccb443eaeaf202072d2d2d
Laura Marshall <L_H_Marshall@yahoo.co.uk>
2013-08-08 15:40:48 +0200

    Fixed bug so that it can construct and object from a pre-defined grid Updated the plot function so it can create a new plot which can rescale the axis from m to km or vice versa

--- DSsim/R/Density.R
+++ DSsim/R/Density.R
@@ -15,13 +15,19 @@ setClass("Density", representation(region.name = "character", strata.name = "cha
 setMethod(
   f="initialize",
   signature="Density",
-  definition=function(.Object, region, strata.name = character(0), density.surface = list(), x.space, y.space, constant = NULL, shapefile = NULL, density.gam = NULL, jit = 1){
+  definition=function(.Object, region, strata.name = character(0), density.surface = list(), x.space, y.space, constant = NULL, density.gam = NULL, jit = 1){
     #Input pre-processing
-    if(length(density.surface) == 0 & !is.null(constant)){
-      density.surface <- get.surface.constant(region, x.space, y.space, constant, jit)
-    }else if(length(density.surface) == 0 & !is.null(density.gam)){
-      density.surface <- get.surface.gam(region, x.space, y.space, gam.model = density.gam)
-    }else{      
+    if(length(density.surface) == 0){
+      if(!is.null(constant)){
+        #Create density surface with constant density within strata
+        density.surface <- get.surface.constant(region, x.space, y.space, constant, jit)
+      }else if(!is.null(density.gam)){
+        #Create density surface from gam
+        density.surface <- get.surface.gam(region, x.space, y.space, gam.model = density.gam)
+      }else{
+        message("Error: You must supply either the density surface, constant or density.gam argument. DSM and formula not currently supported.")
+        return(NULL)      
+      }
     }
     #Set slots
     .Object@region.name <- region@region.name
@@ -75,11 +81,15 @@ setValidity("Density",
 ################################################################################ 
 
 setMethod("plot","Density",
-  function(x, y, ...){
+  function(x, y, add = FALSE, plot.units = character(0), ...){
     density.surface <- x@density.surface
     densities <- NULL
+    x.vals <- NULL
+    y.vals <- NULL
     for(strat in seq(along = density.surface)){
       densities <- c(densities, density.surface[[strat]]$density)
+      x.vals <- c(x.vals, density.surface[[strat]]$x)
+      y.vals <- c(y.vals, density.surface[[strat]]$y)
     }
     zlim <- range(densities) 
     if(zlim[2] - zlim[1] < 1){
@@ -91,23 +101,51 @@ setMethod("plot","Density",
     zlen <- zlim[2] - zlim[1] + 1
     colorlut <- heat.colors(zlen) 
     colorlut <- colorlut[length(colorlut):1]
+    #Set up plot
+    if(length(plot.units) == 0){
+      plot.units <- region@units
+    }
+    if(!add){
+      xlabel <- paste("X-coords (",plot.units[1],")", sep = "")
+      ylabel <- paste("Y-coords (",plot.units[1],")", sep = "")
+      plot(range(x.vals), range(y.vals), col = "white", xlab = xlabel, ylab = ylabel, main = x@region.name, yaxt = "n", xaxt = "n", ...)
+      xticks <- axTicks(1)
+      yticks <- axTicks(2)
+      #Set up axes
+      if(plot.units != region@units){
+        #convert units
+        if(region@units == "m" & plot.units == "km"){ 
+          axis(1, at = xticks, labels = xticks/1000)
+          axis(2, at = yticks, labels = yticks/1000)
+        }else if(region@units == "km" & plot.units == "m"){
+          axis(1, at = xticks, labels = xticks*1000)
+          axis(2, at = yticks, labels = yticks*1000)
+        }else{
+          message("These units are not currently supported.")
+        }
+      }else{
+        #no unit conversion needed
+        axis(1, at = xticks, labels = xticks)
+        axis(2, at = yticks, labels = yticks)
+      }
+    }
     for(strat in seq(along = density.surface)){
       col <- colorlut[density.surface[[strat]]$density*multiplier-zlim[1]+1]
       points(density.surface[[strat]]$x, density.surface[[strat]]$y, col = col, pch = 20)
-    }         
+    }      
   }
 )
 
 
 setMethod("add.hotspot","Density",
-  function(object, centre, radius, amplitude){
+  function(object, centre, sigma, amplitude){
     density.surface <- object@density.surface    
     for(strat in seq(along = density.surface)){
       #Find distances from centre to each point on the density surface
       strata.surface <- density.surface[[strat]]
       dists <- sqrt((strata.surface$x-centre[1])^2 + (strata.surface$y-centre[2])^2) 
       #Calculate radial decay
-      additive.values <- (exp(-dists^2/(2*radius^2)))*amplitude
+      additive.values <- (exp(-dists^2/(2*sigma^2)))*amplitude
       #Add to surface
       strata.surface$density <- strata.surface$density+additive.values
       density.surface[[strat]] <- strata.surface
