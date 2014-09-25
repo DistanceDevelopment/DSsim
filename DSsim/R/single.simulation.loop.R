c9307fadf32d5b3d08441311428148f1fc6f5808
Laura Marshall <L_H_Marshall@yahoo.co.uk>
2014-09-25 16:48:11 -0300

    deals with non-converging models

--- DSsim/R/single.simulation.loop.R
+++ DSsim/R/single.simulation.loop.R
@@ -1,39 +1,33 @@
 
 #single.simulation.loop <- function(i, object){
-single.simulation.loop <- function(i, object, save.data = FALSE, load.data = FALSE, data.path = character(0)){
+single.simulation.loop <- function(i, object, save.data, load.data, data.path = character(0)){
 # Input: i - integer representing the loop number
 #        object - an object of class Simulation
 #
 # Output: the updated Simulation object 
 #
-    #generate population
-    population <- generate.population(object)
-    #generate transects
-    if(!object@single.transect.set){
-      #Selects which set of transects to load
-      object@design@file.index <- i
-    }
-    transects <- generate.transects(object)
-    #make survey object
-    if(object@double.observer){
-      message("Double observer simulations not supported at present")#move this to the checking of the simulation object
-    }else{
-      if(inherits(object@design, "LT.Design")){
-        survey <- new(Class = "Single.Obs.LT.Survey", population = population, line.transect = transects, perp.truncation = object@detectability@truncation)
+    if(!load.data){
+      #generate population
+      population <- generate.population(object)
+      #generate transects
+      if(!object@single.transect.set){
+        #Selects which set of transects to load
+        object@design@file.index <- i
+      }
+      transects <- generate.transects(object)
+      #make survey object
+      if(object@double.observer){
+        message("Double observer simulations not supported at present")#move this to the checking of the simulation object
+      }else{
+        if(inherits(object@design, "LT.Design")){
+          survey <- new(Class = "Single.Obs.LT.Survey", population = population, line.transect = transects, perp.truncation = object@detectability@truncation)
+        }
       }
     }
-#<<<<<<< HEAD
-#    #simulate survey
-#    ddf.data <- survey.data$ddf.data 
-#    survey.data <- create.survey.results(object = survey, dht.tables = TRUE, region = object@region)
-#    obs.table <- survey.data$obs.table
-#    region.table <- survey.data$region.table
-#    sample.table <- survey.data$sample.table
-#    n.in.covered <- survey.data$n.in.covered 
-#=======
     if(load.data){
       #load data
       load(paste(data.path,"dataset_",i,".robj", sep = ""))
+      cat("Analysing dataset: ", i, fill = TRUE)
       ddf.data <- dist.data$ddf 
       obs.table <- dist.data$obs.table
       sample.table <- dist.data$sample.table
@@ -52,22 +46,24 @@ single.simulation.loop <- function(i, object, save.data = FALSE, load.data = FAL
         save(dist.data, file = paste("dataset_",i,".robj", sep = ""))
       }
     }
-#>>>>>>> Binned-Data
-    #analyse survey
-    ddf.results <- run.analysis(object, ddf.data)
     #Find how many animals were in the covered region
     if(length(object@ddf.analyses[[1]]@truncation) > 0){
       n.in.covered <- length(which(n.in.covered <= object@ddf.analyses[[1]]@truncation))  
     }else{
       n.in.covered <- length(n.in.covered)
     }
-    #Store ddf results
-    object@results$Detection <- store.ddf.results(object@results$Detection, ddf.results, i, n.in.covered)
-    #Compute density / abundance estimates
-    compute.dht = TRUE
-    if(compute.dht){
-      dht.results <- dht(ddf.results, region.table@region.table, sample.table@sample.table, obs.table@obs.table)
-      object@results <- store.dht.results(object@results, dht.results, i, object@population.description@size)                                           
+    #analyse survey
+    ddf.results <- run.analysis(object, ddf.data)
+    #Check at least one model worked
+    if(!is.null(ddf.results)){
+      #Store ddf results
+      object@results$Detection <- store.ddf.results(object@results$Detection, ddf.results, i, n.in.covered)
+      #Compute density / abundance estimates
+      compute.dht = TRUE
+      if(compute.dht){
+        dht.results <- dht(ddf.results, region.table@region.table, sample.table@sample.table, obs.table@obs.table)
+        object@results <- store.dht.results(object@results, dht.results, i, object@population.description@size)                                           
+      }
     }
     object@results$filename <- object@design@filenames[object@design@file.index] 
     return(object@results)
