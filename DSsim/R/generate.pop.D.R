c7ccff6bd9bbc3a0f3a56306904536b8a3fd43e9
Laura Marshall <L_H_Marshall@yahoo.co.uk>
2014-12-15 02:01:54 -0300

    Catching N = 0 situations
    Added code to make sure that NA’s are not put in where there are zero
    animals in a strata

--- DSsim/R/generate.pop.D.R
+++ DSsim/R/generate.pop.D.R
@@ -1,6 +1,7 @@
 #generate.pop.D <- function(test, population.description, region.obj){
 generate.pop.D <- function(population.description, region.obj){
   density.obj <- population.description@density
+  first <- TRUE
   for(strat in seq(along = density.obj@density.surface)){
     n.cells <- nrow(density.obj@density.surface[[strat]])
     densities <- density.obj@density.surface[[strat]][["density"]]
@@ -8,31 +9,33 @@ generate.pop.D <- function(population.description, region.obj){
     lambdas <- densities*cell.area
     #generate the number of animals to fall in each cell from a Poisson distribution
     no.in.cells <- rpois(n.cells,  lambda = lambdas)
-    #repeat each row count the number of times in no.in.cell
-    row.ids <- rep(1:n.cells, no.in.cells)
-    #extract grid cells
-    grid.locations <- density.obj@density.surface[[strat]][row.ids,]
-    #generate random locations within grid cell
-    rx <- runif(nrow(grid.locations), -density.obj@x.space/2, density.obj@x.space/2)  
-    ry <- runif(nrow(grid.locations), -density.obj@y.space/2, density.obj@y.space/2)
-    #find x,y coords of animals
-    grid.locations$x.coord <- grid.locations$x+rx
-    grid.locations$y.coord <- grid.locations$y+ry
-    #find which x,y coords are within the region
-    pts <- as.points(grid.locations$x.coord, grid.locations$y.coord) 
-    grid.locations$in.region <- in.polygons(poly.list = region.obj@coords[[strat]], pts = pts, boundary = TRUE)
-    grid.locations$in.gaps   <- in.polygons(poly.list = region.obj@gaps[[strat]], pts = pts, boundary = TRUE)
-    #Find which animals are in the region
-    grid.locations <- grid.locations[grid.locations$in.region,]
-    grid.locations <- grid.locations[!grid.locations$in.gaps,]
-    if(strat == 1){
-      all.grid.locations <- grid.locations
-    }else{
-      all.grid.locations <- rbind(all.grid.locations, grid.locations)
+    #check there are some animals
+    if(sum(no.in.cells) > 0){
+      #repeat each row count the number of times in no.in.cell
+      row.ids <- rep(1:n.cells, no.in.cells)
+      #extract grid cells
+      grid.locations <- density.obj@density.surface[[strat]][row.ids,]
+      #generate random locations within grid cell
+      rx <- runif(nrow(grid.locations), -density.obj@x.space/2, density.obj@x.space/2)  
+      ry <- runif(nrow(grid.locations), -density.obj@y.space/2, density.obj@y.space/2)
+      #find x,y coords of animals
+      grid.locations$x.coord <- grid.locations$x+rx
+      grid.locations$y.coord <- grid.locations$y+ry
+      #find which x,y coords are within the region
+      pts <- as.points(grid.locations$x.coord, grid.locations$y.coord) 
+      grid.locations$in.region <- in.polygons(poly.list = region.obj@coords[[strat]], pts = pts, boundary = TRUE)
+      grid.locations$in.gaps   <- in.polygons(poly.list = region.obj@gaps[[strat]], pts = pts, boundary = TRUE)
+      #Find which animals are in the region
+      grid.locations <- grid.locations[grid.locations$in.region,]
+      grid.locations <- grid.locations[!grid.locations$in.gaps,]
+      if(strat == 1){
+        all.grid.locations <- grid.locations
+      }else{
+        all.grid.locations <- rbind(all.grid.locations, grid.locations)
+      }
     }
   }
   return(all.grid.locations)  
-  #return(nrow(all.grid.locations))
 }
 
 #TESTING
