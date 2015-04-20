85c1bb4dd6a3ee705d2728738c1ef68c2c0045b9
Laura Marshall <L_H_Marshall@yahoo.co.uk>
2015-04-19 22:26:47 -0300

    Now accepts all types of ESRI polygon shapefiles
    codes 5, 15 and 25

--- DSsim/R/check.shapefile.R
+++ DSsim/R/check.shapefile.R
@@ -1,7 +1,9 @@
 check.shapefile <- function(shapefile){
+  #This fuction was added as there were some issues with shapefiles 
+  #created by Distance. There seemed to be some redundant information added.
   keep <- rep(NA, length(shapefile$shp$shp))
   for(i in seq(along=shapefile$shp$shp)){
-    keep[i] <- shapefile$shp$shp[[i]]$shape.type == 5
+    keep[i] <- shapefile$shp$shp[[i]]$shape.type %in% c(5,15,25)
   }
   index <- which(keep)
   new.shp.list <- list()
