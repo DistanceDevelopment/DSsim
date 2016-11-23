f2a3440b250ff081d84979d1aaf8f9ffb36a4aed
Laura Marshall <L_H_Marshall@yahoo.co.uk>
2016-11-23 10:56:44 +0000

    Added function which calculates scale param and stores in the Population object
    This seemed the easiest way to deal with the covariates affecting the
    scale parameter.

--- DSsim/R/Population.Description.R
+++ DSsim/R/Population.Description.R
@@ -125,6 +125,8 @@ setMethod(
     if(length(object@covariates) > 0){
       population.dataframe <- add.covariate.values(population.dataframe, object@covariates)
     }
+    # Add scale parameter values
+    population.dataframe <- calculate.scale.param(population.dataframe, detectability, region.obj)
     # Make population object
     population <- new(Class = "Population", region = object@region.name, strata.names = region.obj@region.name, N = N, D = N/region.obj@area, population = population.dataframe, detectability = detectability)
     return(population)
