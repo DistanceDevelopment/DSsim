052c53fb082978f58453565735a90015ded2f07d
Laura Marshall <L_H_Marshall@yahoo.co.uk>
2016-11-30 22:45:46 +0000

    Added detectability plotting functions

--- DSsim/R/Detectability.R
+++ DSsim/R/Detectability.R
@@ -46,42 +46,212 @@ setMethod(
 )
 
 setValidity("Detectability",
-  function(object){
-    if(!object@key.function%in%c("hr","hn","uf")){
-      return("Unsupported key function")
-    }
-    if(object@key.function == "hr" & length(object@shape.param) == 0){
-      return("You have selected the hazard rate model but not supplied a shape parameter.")
-    }
-    for(i in seq(along = object@scale.param)){
-      if(object@scale.param[i] <= 0){
-        return("Invalid scale parameter. Must be greater than zero.")
-      }else if(object@key.function == "uf" & object@scale.param[i] > 1){
-        return("Invalid scale parameter. Must be greater than zero and less than 1 for the uniform distribution.")
-      } 
-    }
-    for(i in seq(along = object@shape.param)){
-      if(object@shape.param[i] < 0){
-        return("Invalid shape parameter. Must be greater than or equal to zero.")
-      }  
-    }
-    if(length(object@scale.param) > 1 & length(object@shape.param) > 1 & length(object@scale.param) != length(object@shape.param)){
-      return("The same number of values must be provided for both the shape and scale parameters or only one value supplied for either the shape or scale parameter.")
-    }
-    cov.names <- names(object@cov.param)
-    if(any(cov.names == "")){
-      return("Not all the elements of the cov.param list are named. Please provide names for all elements.")
-    }
-    for(cov in seq(along = object@cov.param)){
-      if(length(object@cov.param[[cov]]) == 0){
-        return(paste("List element ", cov.names[cov], " of the cov.param list does not contain any values."), sep = "")
-      }
-    }
-    return(TRUE)
-  }
+            function(object){
+              if(!object@key.function%in%c("hr","hn","uf")){
+                return("Unsupported key function")
+              }
+              if(object@key.function == "hr" & length(object@shape.param) == 0){
+                return("You have selected the hazard rate model but not supplied a shape parameter.")
+              }
+              for(i in seq(along = object@scale.param)){
+                if(object@scale.param[i] <= 0){
+                  return("Invalid scale parameter. Must be greater than zero.")
+                }else if(object@key.function == "uf" & object@scale.param[i] > 1){
+                  return("Invalid scale parameter. Must be greater than zero and less than 1 for the uniform distribution.")
+                } 
+              }
+              for(i in seq(along = object@shape.param)){
+                if(object@shape.param[i] < 0){
+                  return("Invalid shape parameter. Must be greater than or equal to zero.")
+                }  
+              }
+              if(length(object@scale.param) > 1 & length(object@shape.param) > 1 & length(object@scale.param) != length(object@shape.param)){
+                return("The same number of values must be provided for both the shape and scale parameters or only one value supplied for either the shape or scale parameter.")
+              }
+              cov.names <- names(object@cov.param)
+              if(any(cov.names == "")){
+                return("Not all the elements of the cov.param list are named. Please provide names for all elements.")
+              }
+              for(cov in seq(along = object@cov.param)){
+                if(length(object@cov.param[[cov]]) == 0){
+                  return(paste("List element ", cov.names[cov], " of the cov.param list does not contain any values."), sep = "")
+                }
+              }
+              return(TRUE)
+            }
 )
 
 # GENERIC METHODS DEFINITIONS --------------------------------------------
+
+
+#' @exportMethod plot
+setMethod(
+  f="plot",
+  signature="Detectability",
+  definition=function(x, y, add = FALSE, plot.units = character(0), region.col = NULL, gap.col = NULL, main = "", ...){
+    cat("Please provide a population description as well as detectability to enable plotting.")
+  })
+
+
+#' @importFrom graphics lines plot legend
+#' @importFrom stats quantile
+#' @exportMethod plot
+setMethod(
+  f="plot",
+  signature=c("Detectability","Population.Description"),
+  definition=function(x, y, add = FALSE, plot.units = character(0), region.col = NULL, gap.col = NULL, main = "", ...){
+    object <- x
+    pop.desc <- y
+    # Find out how many covariates there are
+    no.covariates <- length(object@cov.param)
+    cov.names <- names(object@cov.param)
+    # set mfrow storing old settings
+    mfrow.value <- switch(no.covariates,
+                          "1" = c(1,1),
+                          "2" = c(1,2),
+                          "3" = c(2,2),
+                          "4" = c(2,2),
+                          "5" = c(2,3),
+                          "6" = c(2,3),
+                          "7" = c(3,3),
+                          "8" = c(3,3),
+                          "9" = c(3,3))
+    # set on exit to return to old settings
+    oldparams <- par(mfrow = mfrow.value)
+    on.exit(par(oldparams))
+    # generate x values to truncation
+    x <- seq(0, object@truncation, length = 200)
+    # get the scale and shape parameters
+    scale.param <- object@scale.param
+    shape.param <- object@shape.param
+    # iterate through all the covariates and make plots
+    for(cov in seq(along = object@cov.param)){
+      # set up initial plot 
+      plot(0,0, xlim = c(0,object@truncation), ylim = c(0,1.2), main = paste("Covariate: ", cov.names[cov], sep = ""), col = "white", xlab = "Distance", ylab = "Detection Probability")
+      cov.params <- object@cov.param[[cov]]
+      if(class(object@cov.param[[cov]]) == "data.frame"){
+        factor = TRUE
+        no.cov.strata <- ifelse(is.null(cov.params$strata), 1, length(unique(cov.params$strata)))
+      }else{
+        factor = FALSE
+        no.cov.strata <- length(cov.params)
+      }
+      no.strata <- max(length(scale.param), length(shape.param), no.cov.strata)
+      # make up storage array
+      if(factor){
+        nlevels <- length(unique(cov.params$level))
+      }else{
+        nlevels <- 3
+      }
+      y <- array(NA, dim = c(nlevels, 200, no.strata))
+      # iterate through strata
+      for(strat in 1:no.strata){
+        # basic scale param
+        if(length(scale.param) == no.strata){
+          scale.param.strat <- scale.param[strat]  
+        }else{
+          scale.param.strat <- scale.param[1]  
+        }
+        # if hazard rate do the same for shape parameter
+        if(object@key.function == "hr"){
+          if(length(shape.param) == no.strata){
+            shape.param.strat <- shape.param[strat]  
+          }else{
+            shape.param.strat <- shape.param[1]  
+          }
+        }
+        if(factor){
+          # get scale adjustment for each level
+          if(!is.null(cov.params$strata)){
+            scale.adjustments <- cov.params$param[cov.params$strata == pop.desc@strata.names[strat]]  
+          }else{
+            scale.adjustments <- cov.params$param
+          }
+          # new scale params
+          new.scale.params <- exp(log(scale.param.strat) + scale.adjustments)
+          # generate y vals
+          for(i in seq(along = new.scale.params)){
+            y[i,,strat] <- switch(object@key.function,
+                                  "hn" = hn.detect(x,new.scale.params[i]),
+                                  "hr" = hr.detect(x,new.scale.params[i], shape.param.strat),
+                                  "uf" = rep(new.scale.params[i], length(x)))
+          }
+        }else{
+          # get 2.5% and 97.5% quantiles for covariate values
+          cov.dist <- pop.desc@covariates[[cov.names[cov]]]
+          if(length(cov.dist) == no.strata){
+            cov.dist <- cov.dist[[strat]]
+          }else{
+            cov.dist <- cov.dist[[1]]
+          }
+          dist.param <- cov.dist[[2]]
+          dist <- cov.dist[[1]]
+          int <- c(0.025, 0.5, 0.975)
+          if(dist == "ztruncpois"){
+            temp <- rtpois(999, mean = dist.param$mean)
+            quantiles <- quantile(temp, int)
+          }else{
+            quantiles <- switch(dist,
+                                  "normal" = qnorm(int, dist.param$mean, dist.param$sd),
+                                  "poisson" = qpois(int, dist.param$lambda),
+                                  "lognormal" = qlnorm(int, dist.param$meanlog, dist.param$sdlog))
+          }
+          # get adjustment paramters
+          if(length(cov.params) == no.strata){
+            strat.param <- cov.params[strat]
+          }else{
+            strat.param <- cov.params[1]
+          }
+          scale.adjustments <- quantiles*strat.param
+          # new scale params
+          new.scale.params <- exp(log(scale.param.strat) + scale.adjustments)  
+          # generate y vals
+          for(i in seq(along = new.scale.params)){
+            y[i,,strat] <- switch(object@key.function,
+                                  "hn" = hn.detect(x,new.scale.params[i]),
+                                  "hr" = hr.detect(x,new.scale.params[i], shape.param.strat),
+                                  "uf" = rep(new.scale.params[i], length(x)))
+          }
+        }
+        if(factor){
+          llty <- seq(along = y[,1,strat])  
+        }else{
+          llty <- c(2,1,2)
+        }
+        # Add detection functions
+        for(i in seq(along = y[,1,strat])){
+          lines(x, y[i,,strat], lty = llty[i], col = strat)
+        }
+      }#loop over strata
+      # Add legend
+      legend.x <- 0.6*object@truncation
+      strata.names <- pop.desc@strata.names
+      if(factor){
+        no.levels <- length(unique(cov.params$level))
+        new.strata.names <- character(0)
+        for(i in seq(along = strata.names)){
+          new.strata.names <- c(new.strata.names, rep(strata.names[i], no.levels))
+        }
+        legend.text <- paste(new.strata.names, ".", rep(unique(cov.params$level), no.strata), sep = "")
+        ccol <- sort(rep(1:no.strata, length(scale.adjustments)))
+        llty <- rep(1:length(scale.adjustments),no.strata)
+        legend(legend.x, 1.2,  col = ccol, lty = llty, legend = legend.text, bty = "n")
+      }else{
+        ccol <- sort(rep(1:no.strata, 2))
+        llty <- rep(c(1,2), no.strata)
+        new.strata.names <- character(0)
+        description <- character(0)
+        for(i in seq(along = strata.names)){
+          new.strata.names <- c(new.strata.names, rep(strata.names[i], 2))
+          description <- c(description, "mean", "95%ints")
+        }
+        legend.text <- paste(new.strata.names, ".", description, sep = "")
+        legend(legend.x, 1.2,  col = ccol, lty = llty, legend = legend.text, bty = "n")
+      }
+    }#loop over covariates
+  })
+
+
 
 
 
